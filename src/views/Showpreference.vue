f145cc331d2af56ed03b8a891c484f15f354cc78
Pipp94 <PKdf1234>
2019-11-19 20:00:05 +0100

    Sortable List added

--- src/views/Showpreference.vue
+++ src/views/Showpreference.vue
@@ -2,94 +2,39 @@
     <div id="show">
         <br>
         <h4><b>Hier hast du nun die Möglichkeit deine persönlichen Daten und Präferenzen anzugeben.
-        Anschließend wirst du auf eine Seite mit einer Auflistung der Wohnorte, angepasst an deine individuellen Eingaben weitergeleitet.</b></h4>
+            Anschließend wirst du auf eine Seite mit einer Auflistung der Wohnorte, angepasst an deine individuellen Eingaben weitergeleitet.</b></h4>
         <br>
         <div class="container" id="box">
             <form>
-                <h5 class="text-uppercase">Persönliche Daten</h5><br>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Name</label>
-                    <div class="col">
-                        <input type="text" class="form-control" placeholder="First name">
-                    </div>
-                    <div class="col">
-                        <input type="text" class="form-control" placeholder="Last name">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="inputage1" class="col-sm-2 col-form-label">Alter</label>
-                    <div class="col-sm-10">
-                        <input type="number" class="form-control" id="inputage1" placeholder="Age">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="inputplace1" class="col-sm-2 col-form-label">Wohnort</label>
-                    <div class="col">
-                        <input type="text" class="form-control" id="inputplace1" placeholder="City">
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Allergien</label>
-                    <div class="col">
-                        <textarea rows="4" cols="50"></textarea>
-                    </div>
-                    <div class="col">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox" value="option1">
-                            <label class="form-check-label" for="inlineCheckbox">Keine Allergien bekannt</label>
-                        </div>
-                    </div>
 
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Anzahl Ergebnisse</label>
-                    <div class="col">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox0" value="option1">
-                            <label class="form-check-label" for="inlineCheckbox0">1</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option1">
-                            <label class="form-check-label" for="inlineCheckbox2">2</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option1">
-                            <label class="form-check-label" for="inlineCheckbox3">3</label>
-                        </div>
-                    </div>
-                </div>
-<br>
                 <h5 class="text-uppercase">Umgebung und Lage</h5><br>
-                <div class="form-group row" >
-                    <label class="col-sm-2 col-form-label">Rating</label>
-                    <div class="col" id="rating">
-                        <table style="width:90%" id="table">
-                            <tr>
-                               <td>1</td>
-                                <td>2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>5</td>
-                                <td>6</td>
-                                <td>7</td>
-                                <td>8</td>
-                                <td>9</td>
-                                <td>10</td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
+
+                <h1>Skills</h1>
+                <draggable v-model="myArray" ghost-class="ghost" @end="onEnd">
+                    <transition-group type="transition" name="flip-list">
+                        <div class="sortable" :id="element.id" v-for="element in myArray" :key="element.id">
+                            <strong>{{element.name}}</strong>
+                        </div>
+                    </transition-group>
+                </draggable>
+
+                <p><strong>Alter Index: </strong> {{oldIndex}}</p>
+                <p><strong>Neuer Index: </strong> {{newIndex}}</p>
+
+
+
+
+
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Gute Busanbindung</label>
                     <div class="vue">
                         <!-- default props -->
                         <vue-slider
                                 ref="slider"
-                                v-model="value"
-                                v-bind="options"
+                                v-model="value1"
 
                         ></vue-slider>
-                        <h4>{{ value }}</h4>
+                        <h4>{{ value1 }}</h4>
                     </div>
 
 
@@ -100,11 +45,10 @@
                         <!-- default props -->
                         <vue-slider
                                 ref="slider"
-                                v-model="value"
-                                v-bind="options"
+                                v-model="value2"
 
                         ></vue-slider>
-                        <h4>{{ value }}</h4>
+                        <h4>{{ value2 }}</h4>
                     </div>
 
                 </div>
@@ -116,11 +60,10 @@
                         <!-- default props -->
                         <vue-slider
                                 ref="slider"
-                                v-model="value"
-                                v-bind="options"
+                                v-model="value3"
 
                         ></vue-slider>
-                        <h4>{{ value }}</h4>
+                        <h4>{{ value3 }}</h4>
                     </div>
 
                 </div>
@@ -134,11 +77,10 @@
                         <!-- default props -->
                         <vue-slider
                                 ref="slider"
-                                v-model="value"
-                                v-bind="options"
+                                v-model="value4"
 
                         ></vue-slider>
-                        <h4>{{ value }}</h4>
+                        <h4>{{ value4 }}</h4>
                     </div>
                 </div>
                 <div class="form-group row">
@@ -147,28 +89,26 @@
                         <!-- default props -->
                         <vue-slider
                                 ref="slider"
-                                v-model="value"
-                                v-bind="options"
+                                v-model="value5"
 
                         ></vue-slider>
-                        <h4>{{ value }}</h4>
+                        <h4>{{ value5 }}</h4>
                     </div>
-                   <!-- <div class="col">
-                        <input type="range" min="1" max="10" value="1" class="slider" id="myRange4">
-                    </div>-->
+                    <!-- <div class="col">
+                         <input type="range" min="1" max="10" value="1" class="slider" id="myRange4">
+                     </div>-->
 
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Kriminalitätsrate</label>
+                    <label class="col-sm-2 col-form-label">Sicherheit</label>
                     <div class="vue">
                         <!-- default props -->
                         <vue-slider
                                 ref="slider"
-                                v-model="value"
-                                v-bind="options"
+                                v-model="value6"
 
                         ></vue-slider>
-                        <h4>{{ value }}</h4>
+                        <h4>{{ value6 }}</h4>
                     </div>
                     <!--<div class="col">
                         <input type="range" min="1" max="10" value="1" class="slider" id="myRange5">
@@ -180,11 +120,10 @@
                         <!-- default props -->
                         <vue-slider
                                 ref="slider"
-                                v-model="value"
-                                v-bind="options"
+                                v-model="value7"
 
                         ></vue-slider>
-                        <h4>{{ value }}</h4>
+                        <h4>{{ value7 }}</h4>
                     </div>
                     <!--<div class="col">
                         <input type="range" min="1" max="10" value="1" class="slider" id="myRange10">
@@ -193,34 +132,34 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label"></label>
                     <div class="col">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox11" value="option1">
-                    <label class="form-check-label" for="inlineCheckbox11">FH Aachen</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="inlineCheckbox12" value="option1">
-                    <label class="form-check-label" for="inlineCheckbox12">RWTH Aachen</label>
-                </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox11" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox11">FH Aachen</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox12" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox12">RWTH Aachen</label>
+                        </div>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Lage</label>
                     <div class="col">
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="option1">
-                        <label class="form-check-label" for="inlineCheckbox4">Innenstadt</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="inlineCheckbox5" value="option1">
-                        <label class="form-check-label" for="inlineCheckbox5">weiter außerhalb</label>
-                    </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox4">Innenstadt</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" id="inlineCheckbox5" value="option1">
+                            <label class="form-check-label" for="inlineCheckbox5">weiter außerhalb</label>
+                        </div>
                     </div>
                 </div>
 
                 <br>
                 <h5 class="text-uppercase">Freizeit</h5><br>
                 <div class="form-group row">
-                    <label for="inputplace1" class="col-sm-2">Sportaktivitäten</label>
+                    <label class="col-sm-2">Sportaktivitäten</label>
                     <div class="col">
                         <div class="form-check form-check-inline">
                             <input class="form-check-input" type="checkbox" id="inlineCheckbox6" value="option1">
@@ -251,13 +190,12 @@
                             <!-- default props -->
                             <vue-slider
                                     ref="slider"
-                                    v-model="value"
-                                    v-bind="options"
+                                    v-model="value8"
 
                             ></vue-slider>
-                            <h4>{{ value }}</h4>
+                            <h4>{{ value8 }}</h4>
                         </div>
-                       <!-- <input type="range" min="1" max="10" value="1" class="slider" id="myRange6">-->
+                        <!-- <input type="range" min="1" max="10" value="1" class="slider" id="myRange6">-->
                     </div>
                 </div>
                 <div class="form-group row">
@@ -267,13 +205,12 @@
                             <!-- default props -->
                             <vue-slider
                                     ref="slider"
-                                    v-model="value"
-                                    v-bind="options"
+                                    v-model="value9"
 
                             ></vue-slider>
-                            <h4>{{ value }}</h4>
+                            <h4>{{ value9 }}</h4>
                         </div>
-                       <!-- <input type="range" min="1" max="10" value="1" class="slider" id="myRange7">-->
+                        <!-- <input type="range" min="1" max="10" value="1" class="slider" id="myRange7">-->
                     </div>
                 </div>
                 <div>
@@ -289,6 +226,58 @@
 
 <script>
 
+    import VueSlider from 'vue-slider-component'
+    import 'vue-slider-component/theme/antd.css'
+    import draggable from 'vuedraggable'
+
+
+    export default
+    {
+        name: 'DragClass',
+
+        components: {draggable, VueSlider},
+
+        methods: {
+            onEnd: function(evt){
+                // eslint-disable-next-line no-console
+                console.log(evt)
+                this.oldIndex = evt.oldIndex;
+                this.newIndex = evt.newIndex;
+            }
+        },
+
+
+
+        data ()
+        {
+            return{
+                value1: 0,
+                value2: 0,
+                value3: 0,
+                value4: 0,
+                value5: 0,
+                value6: 0,
+                value7: 0,
+                value8: 0,
+                value9: 0,
+
+                //Hier auf die Datenbank zugreifen
+                myArray:[
+                    {name:"Philipp", id:0},
+                    {name:"Tamara", id:1},
+                    {name:"Mario", id:2},
+                    {name:"Tobi", id:3},
+                    {name:"Daniel", id:4},
+                    {name:"Jil-Christ", id:5},
+                    {name:"Adrian", id:6},
+                ],
+                oldIndex:'',
+                newIndex:''
+            }
+        }
+
+    }
+
 </script>
 
 <style>
@@ -301,7 +290,6 @@
     .vue h4
     {
         margin-left: 39px;
-
     }
     #custom-handle {
         width: 2em;
@@ -312,15 +300,12 @@
         line-height: 1.6em;
     }
 
-    .form-group
-    {
-
-    }
     .col-md-10
     {
         margin-left:160px;
 
     }
+
     .col-form-label
     {
         bottom:0px;
@@ -331,7 +316,8 @@
         top:0px;
     }
 
-    #show{
+    #show
+    {
         background-color: #c55b4e;
 
     }
@@ -340,7 +326,9 @@
         border: 1px solid #C5281C;
         border-radius: 6px;
     }
-    #box{
+
+    #box
+    {
         border: 1px solid white;
         text-align: left;
         padding-top: 10px;
@@ -351,39 +339,52 @@
         background-color: #C5281C;
 
     }
-#box label{
-    font-variant: small-caps;
-}
-#box input{
-    border: 1px solid #C5281C;
-}
-#rating{
 
-    padding-right: 350px;
-    padding-left: 15px;
-}
-#box h5{
-    font-weight: bold;
-    color: white;
-}
-#show h4{
-    color: white;
-    padding-left: 80px;
-    padding-right: 80px;
-}
+    #box label
+    {
+        font-variant: small-caps;
+    }
 
-#box button{
-    background-color: white;
-    color: black;
-    border-color: white;
-}
-    #link p{
+    #box input
+    {
+        border: 1px solid #C5281C;
+    }
+
+    #rating
+    {
+        padding-right: 350px;
+        padding-left: 15px;
+    }
+
+    #box h5
+    {
+        font-weight: bold;
+        color: white;
+    }
+
+    #show h4
+    {
+        color: white;
+        padding-left: 80px;
+        padding-right: 80px;
+    }
+
+    #box button
+    {
+        background-color: white;
+        color: black;
+        border-color: white;
+    }
+
+    #link p
+    {
         color: white;
     }
 
     /*Slider Hintergrund */
 
-    .slider {
+    .slider
+    {
         -webkit-appearance: none;
         width: 40%;
         top:5px;
@@ -396,11 +397,13 @@
         transition: opacity .2s;
     }
 
-    .slider:hover {
+    .slider:hover
+    {
         opacity: 1;
     }
 
-    .slider::-webkit-slider-thumb {
+    .slider::-webkit-slider-thumb
+    {
         -webkit-appearance: none;
         appearance: none;
         width: 25px;
@@ -409,10 +412,43 @@
         cursor: pointer;
     }
 
-    .slider::-moz-range-thumb {
+    .slider::-moz-range-thumb
+    {
         width: 25px;
         height: 25px;
         background: #000000;
         cursor: pointer;
     }
+
+    /*Draggable*/
+
+    strong
+    {
+        display: flow;
+    }
+
+    .sortable
+    {
+        width: 50%;
+        background: white;
+        padding: 1em;
+        cursor: move;
+        margin-bottom: 2px;
+        color: #C5281C;
+    }
+
+    .flip-list
+    {
+        transition: transform 0.5s;
+    }
+
+    .ghost
+    {
+        border-left: 6px solid rgb(0, 0, 0);
+        box-shadow: 10px 10px 5px -1px rgb(0, 0, 0);
+        opacity: .7;
+    }
+
+
+
 </style>
