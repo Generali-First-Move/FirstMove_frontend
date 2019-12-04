33a049a0845d0faa96bf72f5db5ba3c04bee651f
Inexus <adrian.hoeppener@alumni.fh-aachen.de>
2019-12-04 18:34:47 +0100

    Google Map Change Window Implementation Alphaversion

--- src/main.js
+++ src/main.js
@@ -3,17 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/sprites/solid.svg'
-import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 import VueSlider from 'vue-slider-component'
 import 'vue-slider-component/theme/default.css'
-import * as VueGoogleMaps from "vue2-google-maps";
+import * as VueGoogleMaps from 'vue2-google-maps';
+import Vue from 'vue';
+
+import Map from './components/map.vue';
+
 import Slider from '@jeremyhamm/vue-slider'
 
 Vue.component('VueSlider', VueSlider);
 require("dotenv").config();
+
+require('style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap');
 
 Vue.use(VueGoogleMaps, {
   load: { key: "AIzaSyD5UnUjfGRvPefBbTgLr-iy3CZ5BD99dBg",
